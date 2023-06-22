8cc1a52e5fff50c37a73e312611db57a2bbcace1
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-06-22 11:22:35 +0600

    Adding mongodb

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zndhpn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    const usersCollection = client.db('mensParlour').collection('users');
+    
+    app.post('/users',async(req,res)=>{
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    }) 
+} finally {
+    
+  }
+}
+run().catch(console.dir);
